3d4e49683d17f703a7253ec2f8a19195d109963e
malsharif <hggih;fv10>
2020-10-12 19:57:39 +0200

    Auth

--- src/main/java/com/clone/reddit/controller/AuthController.java
+++ src/main/java/com/clone/reddit/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.clone.reddit.controller;
 
 import com.clone.reddit.dto.RegisterRequest;
 import com.clone.reddit.service.AuthService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,10 @@ import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
 
-    private AuthService authService;
+    private final AuthService authService;
 
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
